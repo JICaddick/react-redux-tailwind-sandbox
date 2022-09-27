7491269a263a80ccd8614e6546668bcf7e614335
JICaddick <james_caddick16@hotmail.com>
2022-09-27 16:18:00 -0500

    buttons put back to normal. Edits for desktop version finished. Is the dynamic, is it not? Change the size of the screen to find out (perhaps hit refresh too for the image's sake).

--- src/App.js
+++ src/App.js
@@ -9,32 +9,36 @@ function App() {
 	
 return (
 	<div className=" bg-primary-cream h-screen w-screen flex justify-center items-center pb-12">
-		<div className="border-2 rounded-xl mx-6 mt-12 h-full bg-white md:flex lg:flex">
-			<img className="rounded-t-xl sm:h-5/6" src={imageFile} alt="perfume for ad" />
+		<div className="border-2 rounded-xl mx-6 mt-12 bg-white md:flex lg:flex">
+			<img className="rounded-l-xl sm:h-5/6" src={imageFile} alt="perfume for ad" />
 			<div className="">
-				<div className="w-full h-1/6 text-neutral-dark-grayish-blue font-['Monserrat] pl-8 mt-6">
+				<div className="w-full h-1/8 text-neutral-dark-grayish-blue font-['Monserrat] pl-8 mt-6 md:pl-16 md:mt-10 md:text-2xl ">
 					P E R F U M E
 				</div>
 
-				<div className="text-3xl pl-8 pb-2 w-full h-1/6 font-bold font-['fraunces']">
+				<div className="text-3xl md:text-6xl pl-8 pb-2 w-full font-bold font-['fraunces'] md:pl-16 md:py-4">
 					Gabrielle Essence Eau De Parfum
 				</div>
 
-				<div className="w-full h-1/6 text-sm relative p-1/2 text-neutral-dark-grayish-blue pl-8">
+				<div className="w-full h-3/8 text-sm text-neutral-dark-grayish-blue md:my-8 md:pl-16 md:text-4xl">
 					A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
 					for the House of CHANEL.
 				</div>
-				<div className="flex flex-row p-5 items-center justify-between">
-					<div className="w-full text-3xl pl-3 text-primary-dark-cyan font-['fraunces']">
+				<div className="flex flex-row p-5 items-center justify-between md:pl-16">
+					<div className="w-full text-3xl pl-3 text-primary-dark-cyan font-['fraunces'] md:text-5xl">
 						$149.99
 					</div>
-					<div className="w-full line-through text-neutral-dark-grayish-blue">169.99</div>
+					<div className="w-full line-through text-neutral-dark-grayish-blue md:text-2xl">
+						169.99
+					</div>
 				</div>
-				<nav className="flex items-center justify-center bg-yellow-500 text-white border-1/2 text-sm p-1/2 mx-8 font-['Monserrat'] rounded-md h-7">
+				<nav className="flex items-center justify-center bg-yellow-500 text-white border-1/2 text-sm p-1/2 mx-8 font-['Monserrat'] rounded-md h-14">
+					
+					{/* // className="flex items-center justify-center bg-yellow-500 text-white border-1/2 text-sm font-['Monserrat'] rounded-md h-9 md:w-fit md:text-2xl md:px-8"> */}
 					<Link to="/voucher">🏆 Voucher 🏆</Link>
 				</nav>
 				<br></br>
-				<nav className="flex items-center justify-center bg-green-700 text-white border-1/2 text-sm p-1/2 mx-8 font-['Monserrat'] rounded-md h-14">
+				<div className="flex items-center justify-center bg-green-700 text-white border-1/2 text-sm p-1/2 mx-8 font-['Monserrat'] rounded-md h-14">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -50,7 +54,7 @@ return (
 						/>
 					</svg>
 					Add to Cart
-				</nav>
+				</div>
 			</div>
 		</div>
 	</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,11 @@ module.exports = {
 		},
 		fontSize: {
 			'xs': '.75rem',
-			'3xl': '1.875rem',
+			'2xl': '1.563rem',
+			'3xl': '1.953rem',
+			'4xl': '2.441rem',
+			'5xl': '3.052rem',
+			'6xl': '5.5rem',
 		},
 	},
 
